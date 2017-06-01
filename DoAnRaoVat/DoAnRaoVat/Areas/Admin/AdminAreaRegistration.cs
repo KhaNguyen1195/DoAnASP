75abc06a27ef01febad4715690cd699bb7d6e09b
KhaNguyen <KhaNguyen@KHANGUYEN>
2017-05-31 21:43:47 -0700

    Chỉnh sửa phân quyền admin và user

--- DoAnRaoVat/DoAnRaoVat/Areas/Admin/AdminAreaRegistration.cs
+++ DoAnRaoVat/DoAnRaoVat/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace DoAnRaoVat.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index",controller="Home", id = UrlParameter.Optional },
+                new string[] { "DoAnRaoVat.Areas.Admin.Controllers" }
             );
         }
     }
