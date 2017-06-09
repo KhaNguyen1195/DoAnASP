b6ad33fa112e7131f34b851a9f0fdbb4b68c9ba2
QuyPhi <QuyPhi@NONAME>
2017-06-09 19:05:56 +0700

    feedback admin

---
+++ DoAnRaoVat/DoAnRaoVat/Areas/Admin/Controllers/FeedBackController.cs
@@ -0,0 +1,81 @@
+﻿using Model.Dao;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnRaoVat.Areas.Admin.Controllers
+{
+    public class FeedBackController : BaseController
+    {
+        // GET: Admin/FeedBack
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
+        {
+            var dao = new FeedBackDao();
+            var model = dao.LissAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FeedBack feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new FeedBackDao();
+                long id = dao.Insert(feedback);
+                if (id > 0)
+                {
+                    return RedirectToAction("Index", "FeedBack");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm khu vực không thành công");
+                }
+            }
+            return View("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var feedback = new FeedBackDao().ViewDetail(id);
+            return View(feedback);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(FeedBack feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new FeedBackDao();
+                var result = dao.Update(feedback);
+                if (result)
+                {
+                    return RedirectToAction("Index", "FeedBack");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật khu vực không thành công");
+                }
+            }
+            return View("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var dao = new FeedBackDao();
+            dao.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        DoAnASPDBContext db = new DoAnASPDBContext();
+    }
+}
---
+++ DoAnRaoVat/Model/Dao/FeedBackDao.cs
@@ -0,0 +1,79 @@
+﻿using Model.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class FeedBackDao
+    {
+        DoAnASPDBContext db = null;
+        public FeedBackDao()
+        {
+            db = new DoAnASPDBContext();
+        }
+
+        public long Insert(FeedBack entity)
+        {
+            db.FeedBacks.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public FeedBack GetByID(string Name)
+        {
+            return db.FeedBacks.SingleOrDefault(x => x.Name == Name);
+        }
+
+        public FeedBack ViewDetail(int id)
+        {
+            return db.FeedBacks.Find(id);
+        }
+
+        public IEnumerable<FeedBack> LissAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<FeedBack> model = db.FeedBacks;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Name.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+
+        public bool Update(FeedBack entity)
+        {
+            try
+            {
+                var feedback = db.FeedBacks.Find(entity.ID);
+                feedback.Name = entity.Name;
+                feedback.Phone = entity.Phone;
+                feedback.Email = entity.Email;
+                feedback.Address = entity.Address;
+                feedback.Content = entity.Content;
+                feedback.Status = entity.Status;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public bool Delete(int id)
+        {
+            var feedback = db.FeedBacks.SingleOrDefault(x => x.ID == id);
+            if (feedback == null)
+            {
+                return false;
+            }
+            db.FeedBacks.Remove(feedback);
+            db.SaveChanges();
+            return true;
+        }
+    }
+}
