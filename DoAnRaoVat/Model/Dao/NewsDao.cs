e8484cfee30ab073f656face0457e0cf4500ea6b
QuyPhi <QuyPhi@NONAME>
2017-06-17 21:08:53 +0700

    News

--- DoAnRaoVat/Model/Dao/NewsDao.cs
+++ DoAnRaoVat/Model/Dao/NewsDao.cs
@@ -1,4 +1,5 @@
 ﻿using Model.EF;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,60 @@ namespace Model.Dao
             db.News.Add(entity);
             db.SaveChanges();
             return entity.ID;
+        }
+
+        public News GetByID(string Name)
+        {
+            return db.News.SingleOrDefault(x => x.Name == Name);
+        }
+
+        public News ViewDetail(int id)
+        {
+            return db.News.Find(id);
+        }
+        
+        public IEnumerable<News> LissAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<News> model = db.News;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Name.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+
+        public bool Update(News entity)
+        {
+            try
+            {
+                var news = db.News.Find(entity.ID);
+                news.Name = entity.Name;
+                news.Description = entity.Description;
+                news.Price = entity.Price;
+                news.ProductID = entity.ProductID;
+                news.CityID = entity.CityID;
+                //product.ModifiedDate = (DateTime.Now);
+                news.Status = entity.Status;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public bool Delete(int id)
+        {
+            var news = db.News.SingleOrDefault(x => x.ID == id);
+            if (news == null)
+            {
+                return false;
+            }
+            db.News.Remove(news);
+            db.SaveChanges();
+            return true;
         }
     }
 }
